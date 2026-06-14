7abd6e22023e74c36ecd3992179ef2a05b583a69
EX3-0 <dragonliniko@gmail.com>
2026-06-14 18:53:01 -0400

    Even more junk file deletion

--- wc3launcher/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set "PATH=C:\msys64\mingw32\bin;%PATH%"
-cargo build --target i686-pc-windows-gnu
-if errorlevel 1 exit /b %errorlevel%
-copy /Y "target\i686-pc-windows-gnu\debug\wc3launcher.exe" "D:\AgentCoding\WC3Extender\deploy\wc3extender\wc3launcher.exe"
